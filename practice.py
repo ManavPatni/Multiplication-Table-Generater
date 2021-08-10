f8133183a05ad22c2ea5834944d089070bf9c7c5
ManavPatni <87895188+ManavPatni@users.noreply.github.com>
2021-08-10 09:29:48 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,4 @@
+num = int(input("Enter the any number here for which you want the multipilation table- "))
+num2 = int(input("Enter how many times it should be multiplied- "))
+for z in range(1,num2+1):
+    print(num, "*", z, "=", num * z )
